f4596eb2eb0e8492a429ac66a076700375d381a0
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2024-04-15 21:59:25 +0800

    add shadcn

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,4 +2,5 @@ import {sql} from "@vercel/postgres"
 import {drizzle} from "drizzle-orm/vercel-postgres"
 import * as schema from "./schema"
 
+// CLIENT **********************************************************************************************************************************
 export const db = drizzle(sql, {schema})
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,23 +1,15 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
 import {sql} from "drizzle-orm"
 import {index, pgTableCreator, serial, timestamp, varchar} from "drizzle-orm/pg-core"
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 export const createTable = pgTableCreator((name) => `theodosis_${name}`)
 
-export const iamges = createTable(
+// IMAGES **********************************************************************************************************************************
+export const images = createTable(
   "image",
   {
     id: serial("id").primaryKey(),
     name: varchar("name", {length: 256}).notNull(),
-    url: varchar("name", {length: 1024}).notNull(),
+    url: varchar("url", {length: 1024}).notNull(),
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
@@ -27,3 +19,21 @@ export const iamges = createTable(
     nameIndex: index("name_idx").on(example.name),
   })
 )
+
+// SERVICES ********************************************************************************************************************************
+// export const services = createTable(
+//   "service",
+//   {
+//     id: serial("id").primaryKey(),
+//     name: varchar("name", {length: 256}).notNull(),
+//     url: varchar("url", {length: 1024}).notNull(),
+//     createdAt: timestamp("created_at")
+//       .default(sql`CURRENT_TIMESTAMP`)
+//       .notNull(),
+//     updatedAt: timestamp("updatedAt"),
+//   },
+//   (example) => ({
+//     nameIndex: index("name_idx").on(example.name),
+//   })
+// )
+
