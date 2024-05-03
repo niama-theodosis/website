357717c20bcaad95fbacb6cb6e12908c217952fe
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2024-05-03 20:01:50 +0800

    Update service-card.tsx

--- src/components/service-card.tsx
+++ src/components/service-card.tsx
@@ -10,7 +10,7 @@ export default function ServiceCard({service: {excerpt, image, name, slug, uri}}
   return (
     <Card className="flex flex-col">
       <CardHeader className="p-4">
-        <Image src={image.url} alt={name} width={1024} height={1024} className="aspect-video rounded-2xl object-cover" />
+        <Image src={image.url} alt={name} width={550} height={550} className="aspect-video rounded-2xl object-cover" />
       </CardHeader>
       <CardContent className="flex-1 space-y-4 p-4 text-center">
         <h3 className="font-heading text-lg font-bold">{name}</h3>
