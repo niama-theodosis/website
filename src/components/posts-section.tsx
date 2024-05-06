531638df76c9cf0d6f574910d72cecec2c0f297b
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2024-05-06 21:30:28 +0800

    Update posts-section.tsx

--- src/components/posts-section.tsx
+++ src/components/posts-section.tsx
@@ -1,9 +1,9 @@
-import PostCard, {PostCardFragment, PostNone, PostSkeleton} from "@/components/post-card"
+import PostCard, {PostCardFragment, PostNone, PostSkeleton, type PostCardData} from "@/components/post-card"
 import {env} from "@/env"
 import {hashnode} from "@/lib/hashnode"
 import {PageInfoFragment} from "@/lib/hashnode/fragments"
 import {graphql} from "@/lib/hashnode/graphql"
-import {unstable_noStore} from "next/cache"
+import {print} from "graphql"
 import {Suspense, forwardRef} from "react"
 import {Section, SectionContent, SectionHeader, SectionMain, SectionTagline, SectionTitle, type SectionProps} from "./ui/section"
 
@@ -48,10 +48,28 @@ const PostsByTagQuery = graphql(
   [PageInfoFragment, PostCardFragment]
 )
 
+// const fetchPosts = async ({after, first, tag}: Pick<PostsSectionProps, "after" | "first" | "tag">) => {
+//   const data = await (tag
+//     ? hashnode.request(PostsByTagQuery, {host: env.HASHNODE_PUBLICATION_HOST, after, first, tag})
+//     : hashnode.request(PostsQuery, {host: env.HASHNODE_PUBLICATION_HOST, after, first}))
+//   await new Promise((resolve) => setTimeout(resolve, 4000))
+//   return data.publication?.posts.edges ?? []
+// }
+
 const fetchPosts = async ({after, first, tag}: Pick<PostsSectionProps, "after" | "first" | "tag">) => {
   const data = await (tag
     ? hashnode.request(PostsByTagQuery, {host: env.HASHNODE_PUBLICATION_HOST, after, first, tag})
-    : hashnode.request(PostsQuery, {host: env.HASHNODE_PUBLICATION_HOST, after, first}))
+    : fetch(env.HASHNODE_GQL_ENDPOINT, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+          query: print(PostsQuery),
+          variables: {host: env.HASHNODE_PUBLICATION_HOST, after, first},
+        }),
+        cache: "no-store",
+      })
+        .then((res) => res.json() as unknown as {data: {publication: {posts: {edges: {node: PostCardData}[]}}}})
+        .then((res) => res.data))
   return data.publication?.posts.edges ?? []
 }
 
@@ -79,7 +97,6 @@ PostsSection.displayName = "PostsSection"
 
 // ITEMS ***********************************************************************************************************************************
 async function Posts(props: Pick<PostsSectionProps, "after" | "first" | "tag">) {
-  unstable_noStore()
   const posts = await fetchPosts(props)
 
   if (posts.length === 0) return <PostNone />
